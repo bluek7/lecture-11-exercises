beae21ed2dff9998f3c5d3f582213b532444804d
Kush Gupta <bluek7@D-173-250-176-78.dhcp4.washington.edu>
2016-02-10 15:20:06 -0800

    -

--- exercise3.R
+++ exercise3.R
@@ -1,5 +1,7 @@
 ### Exercise 3 ###
 library(jsonlite)
+library(dplyr)
+library(plotly)
 
 # Read in this police shooting JSON data 
 # https://raw.githubusercontent.com/mkfreeman/police-shooting/master/data/response.json
@@ -14,7 +16,29 @@ data <- data %>%
         mutate(shots_fired = ifelse(is.na(shots_fired), mean(shots_fired, na.rm = T), shots_fired))
 
 # Create a bubble map of the data
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showland = TRUE,
+  landcolor = toRGB("gray85"),
+  subunitwidth = 1,
+  countrywidth = 1,
+  subunitcolor = toRGB("white"),
+  countrycolor = toRGB("white")
+)
+
+plot_ly(data, lon = lng, lat = lat, type = 'scattergeo') %>% 
+  layout(title = 'Crowdsource Shootings by Cops', geo = g)
 
 ### Bonus: create informative hover text ###
+data$hover <- paste(data[['Victim Name']], data[['Victim\'s Age']])
+plot_ly(data, 
+        lon = lng, 
+        lat = lat, 
+        marker = list(size = shots_fired / 3 + 1, color = toRGB('red')),
+        type = 'scattergeo',
+      
+        text = hover) %>% 
+  layout(title = 'Crowdsource Shootings by Cops', geo = g)
 
 ### Bonus: Use multiple colors ###
